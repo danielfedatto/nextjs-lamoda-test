6681bfbdff89f38e79094b62f080a1806fce3c38
danielfedatto <daniel.fedatto@gmail.com>
2026-01-31 11:04:12 -0300

    feat(components): implement header component
    - create new Header component file
    - set up basic structure including store title and Cart integration
    - use async component definition

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import Cart from '@/components/Cart/Cart';
+
+const Header = async () => (
+  <header className="bg-zinc-100 py-10 px-10">
+    <div className="@container flex justify-between items-center">
+      <h1 className="text-black text-2xl font-bold">STORE</h1>
+      <Cart />
+    </div>
+  </header>
+);
+
+export default Header;
